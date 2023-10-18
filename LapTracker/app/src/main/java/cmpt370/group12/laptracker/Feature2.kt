665556584d9275edc8cc4dd7e68e80f75f74ffb4
Riley <rileykensmith@gmail.com>
2023-10-17 22:57:08 -0600

    Added a button to show location for debugging purposes

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature2.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature2.kt
@@ -2,19 +2,26 @@ package cmpt370.group12.laptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import android.annotation.SuppressLint
+import androidx.compose.ui.unit.dp
 import cmpt370.group12.laptracker.ui.theme.LapTrackerTheme
+import com.google.android.gms.location.LocationServices
 
 class Feature2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var fusedClient = LocationServices.getFusedLocationProviderClient(this)
+        var loc = LocationClient(this, this.parent, fusedClient)
+        var flow = loc.getLocationFlow(0.1)
         setContent {
             LapTrackerTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,6 +30,11 @@ class Feature2 : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting3("Feature 2")
+                }
+                Spacer(modifier = Modifier.padding(1.dp))
+                Button(onClick = {
+                }){
+                    Text(text = "$flow.toString()")
                 }
             }
         }
