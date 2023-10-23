1facd7952dd60fd34b0b0bb8fdc19657214d6414
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-10-23 04:09:21 -0600

    Can Activiate a Map.  DO NOT USE IN A SURFACE.  this will brick it, this took me until now to figure it out, i just got lucky and dropped a mapcreen on top of a surface and it worked. then i realized the surface doesn't jive with google maps

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature7.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature7.kt
@@ -3,28 +3,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.annotation.SuppressLint
-import cmpt370.group12.laptracker.ui.theme.LapTrackerTheme
+import cmpt370.group12.laptracker.ui.presentation.MapScreen
+
 
 class Feature7 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LapTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting8("Feature 7")
-                }
-            }
+
+             MapScreen()
+
         }
     }
 }
@@ -40,7 +35,7 @@ fun Greeting8(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview8() {
-    LapTrackerTheme {
+
         Greeting8("Android")
-    }
+
 }
