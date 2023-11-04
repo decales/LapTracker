b2cd2e4757d039586dfe2a46f73cc953b037df9e
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-03 23:47:47 -0600

    Added RunTimeEntity Updated RunEntity to include startTime and EndTime

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerEntities.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerEntities.kt
@@ -31,3 +31,49 @@ data class CommentEntity(
     //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
     val timestamp: Timestamp
 )
+data class AchievementEntity(
+    //id : This is the Primary Key For Database Entry into Table AchievementEntity
+    @PrimaryKey
+    val id: Int? = null,
+    //name is the Title of the achievement
+    val name: String,
+    //description full description of the achievement
+    val description: String,
+    //achieved is a boolean that is asserted when the goal is met
+    val achieved: Boolean,
+    //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
+    val timestamp: Timestamp
+)
+data class TrackEntity(
+    //id : This is the Primary Key For Database Entry into Table AchievementEntity
+    @PrimaryKey
+    val id: Int? = null,
+    //name is the Title of the achievement
+    val name: String
+
+)
+data class RunEntity(
+    //id : This is the Primary Key For Database Entry into Table RunEntity
+    @PrimaryKey
+    val id: Int? = null,
+    // fromTrackId: This is a reference to the primary Key "id" for the TrackEntity Table
+    val fromTrackId: Int,
+    // starTime,endTime   these are the timestamps for the start and end of a race
+    // all times can be calculated by taking these times and comparing them to
+    // RunTimeEntities
+    //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
+    val startTime: Timestamp,
+    val endTime: Timestamp
+)
+data class RunTimeEntity(
+    //id : This is the Primary Key For Database Entry into Table RunTimeEntity
+    @PrimaryKey
+    val id: Int? = null,
+    // fromRunId: This is a reference to the primary Key "id" for the RunEntity Table
+    val fromRunId: Int,
+    // fromMapPointId: This is a reference to the primary Key "id" for the MapPointEntity Table
+    val fromMapPointId: Int,
+    // timestamp: this is the timestamp when this point was reached in the run
+    //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
+    val timestamp: Timestamp
+)
