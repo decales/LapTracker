54e7062c4ef2ce4879f5ce9ea43fbb950350918a
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-03 23:09:39 -0600

    Updated The MapPointsEntity To Represent What was Discussed in the Group

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerEntities.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerEntities.kt
@@ -5,9 +5,17 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class MapPointEntity(
-    val lat: Double,
-    val lng: Double,
-    @PrimaryKey val id: Int? = null,
-    val mapid: Int
-
+    //id : This is the Primary Key For Database Entry into Table MapPointEntity
+    @PrimaryKey
+    val id: Int? = null,
+    // fromTrackId: This is a reference to the primary Key "id" for the TrackEntity Table
+    val fromTrackId: Int,
+    // latitude and longitude points for the Point Being Saved
+    val latitude: Double,
+    val longitude: Double,
+    // (Future Use: name of the point or ""
+    val name: String = "",
+    // sequenceNumber: The Order of the Point in a Track, This Will Later Be Used For Sorting Points
+    // on the track
+    val sequenceNumber: Int
 )
