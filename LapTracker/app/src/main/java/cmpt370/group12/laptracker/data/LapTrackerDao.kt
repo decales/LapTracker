0ee7da938c876937b35e3096a7fb1fefcab27272
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-04 00:21:27 -0600

    Added Comments and Achievements to Dao comments can : insertComment, deleteComment,getComments acheivements can: insertAchievements, deleteAchievements, setAchievement,getfinishedAchievements,getunfinishedAchievements,getallAchievements

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerDao.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/LapTrackerDao.kt
@@ -4,14 +4,40 @@ import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MapPointDao {
+interface LapTrackerDao {
 
+    //Comment Table
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComment(spot: CommentEntity)
+    @Delete
+    suspend fun deleteComment(spot: CommentEntity)
+
+    //todo: I probably want to return the Comments sorted by timeStamp
+    @Query("SELECT * FROM commententity WHERE fromTrackId =:currentTrackId")
+    fun getComments(currentTrackId: Int): Flow<List<CommentEntity>>
+
+    //MapPoint Table
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMapPoint(spot: MapPointEntity)
-
     @Delete
     suspend fun deleteMapPoint(spot: MapPointEntity)
 
-    @Query("SELECT * FROM mappointentity WHERE mapid =:idofmap")
-    fun getMapPoints(idofmap: Int): Flow<List<MapPointEntity>>
+    //todo: I probably want to return the map points sorted by sequenceNumber
+    @Query("SELECT * FROM mappointentity WHERE fromTrackId =:currentTrackId")
+    fun getMapPoints(currentTrackId: Int): Flow<List<MapPointEntity>>
+
+    //Achievement Table
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAchievement(spot: AchievementEntity)
+    @Delete
+    suspend fun deleteAchievement(spot: AchievementEntity)
+    @Update
+    suspend fun setAchievement(spot: AchievementEntity)
+    @Query("SELECT * FROM achievemententity WHERE achieved =true")
+    fun getfinishedAchievements(): Flow<List<AchievementEntity>>
+    @Query("SELECT * FROM achievemententity WHERE achieved =false")
+    fun getunfinishedAchievements(): Flow<List<AchievementEntity>>
+    @Query("SELECT * FROM achievemententity")
+    fun getallAchievements(): Flow<List<AchievementEntity>>
+
 }
