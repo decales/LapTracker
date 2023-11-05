2ddff2fa21e64d0988977edd07789775e08ac46e
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-05 17:47:09 -0600

    Finally Was Able To Get the Components All Upto Date without breaking things Whatever Bug was in whatever package, it's gone.

--- LapTracker/app/build.gradle.kts
+++ LapTracker/app/build.gradle.kts
@@ -2,18 +2,19 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("kotlin-parcelize")
     id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "cmpt370.group12.laptracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "cmpt370.group12.laptracker"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -43,9 +44,9 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -53,45 +54,47 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.core:core-ktx:1.10.0")
-    implementation ("androidx.compose.ui:ui:1.4.3")
-    implementation ("androidx.compose.material:material:1.4.3")
+    //implementation ("androidx.core:core-ktx:1.10.0")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.compose.ui:ui:1.5.4")
+    implementation ("androidx.compose.material:material:1.5.4")
     //implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.6.0"))
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
 
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation ("androidx.activity:activity-compose:1.7.0")
+    implementation ("androidx.activity:activity-compose:1.8.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.4.3")
-    debugImplementation ("androidx.compose.ui:ui-tooling:1.4.3")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.5.4")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.5.4")
 
     // Compose dependencies
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
-    implementation ("com.google.maps.android:maps-compose:1.0.0")
-
+    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
+    //implementation ("com.google.maps.android:maps-compose:1.0.0")
+    implementation ("com.google.maps.android:maps-compose:3.1.1")
     // Google Maps
-    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
 
     //Dagger - Hilt
-    implementation ("com.google.dagger:hilt-android:2.45")
-    kapt ("com.google.dagger:hilt-android-compiler:2.45")
+    implementation ("com.google.dagger:hilt-android:2.48")
+    ksp ("com.google.dagger:hilt-android-compiler:2.48")
+    //ksp ("com.google.dagger:hilt-android-compiler:2.48")
     //implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     //kapt ("androidx.hilt:hilt-compiler:1.0.0")
     //implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Room
-    implementation ("androidx.room:room-runtime:2.5.2")
-    kapt ("androidx.room:room-compiler:2.5.2")
+    implementation ("androidx.room:room-runtime:2.6.0")
+    ksp ("androidx.room:room-compiler:2.6.0")
 
     // Kotlin Extensions and Coroutines support for Room
-    implementation ("androidx.room:room-ktx:2.5.2")
+    implementation ("androidx.room:room-ktx:2.6.0")
 }
 
 allprojects {
