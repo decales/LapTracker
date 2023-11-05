1e17f6771b65f41c2a6cf2bd4b50ae1048d2e9cf
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-05 17:48:20 -0600

    ..

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/ui/presentation/MapScreen.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/ui/presentation/MapScreen.kt
@@ -1,20 +1,22 @@
 package cmpt370.group12.laptracker.ui.presentation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.UiSettings
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.maps.android.compose.MarkerState
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnusedMaterialScaffoldPaddingParameter")
 
@@ -24,46 +26,54 @@ fun MapScreen(
     viewModel: MapsViewModel = viewModel()
 ) {
     //val scaffoldState = rememberScaffoldState()
-    val uiSettings = remember {
-        MapUiSettings(zoomControlsEnabled = false)
-    }
+
     Scaffold(
         //scaffoldState = scaffoldState,
         floatingActionButton = {
             FloatingActionButton(onClick = {
-                viewModel.onEvent(MapEvent.ToggleMap)
+                viewModel.onEvent(AppEvent.ToggleMap)
             }) {
 
             }
         }
     ) {
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            properties = viewModel.state.properties,
-            uiSettings = uiSettings,
-            onMapLongClick = {
-                viewModel.onEvent(MapEvent.OnMapLongClick(it))
-            }
-        ) {
-            viewModel.state.mapPoints.forEach { mappoint ->
-                Marker(
-                    position = LatLng(mappoint.lat, mappoint.lng),
-                    title = "(${mappoint.lat}, ${mappoint.lng})",
-                    snippet = "Long click to delete",
-                    onInfoWindowLongClick = {
-                        viewModel.onEvent(
-                            MapEvent.OnInfoWindowLongClick(mappoint)
+        Column(){
+           Row(){ Text(text = "bbbbbbbbbbbbbbbbbcvccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc")}
+           Row() {
+
+            GoogleMap(
+                modifier = Modifier
+                    .height(500.dp),
+
+                properties = viewModel.mapstate.value.properties,
+                uiSettings = viewModel.mapstate.value.uiSettings,
+                onMapLongClick = {
+                    viewModel.onEvent(AppEvent.OnMapLongClick(it))
+                }
+            ) {
+
+                viewModel.trackstate.value.mapPoints.forEach { mappoint ->
+                    Marker(
+                        state = MarkerState(LatLng(mappoint.latitude, mappoint.longitude)),
+                        title = "(${mappoint.latitude}, ${mappoint.longitude})",
+                        snippet = "Long click to delete",
+                        onInfoWindowLongClick = {
+                            viewModel.onEvent(
+                                AppEvent.OnInfoWindowLongClick(mappoint)
+                            )
+                        },
+                        onClick = {
+                            it.showInfoWindow()
+                            true
+                        },
+                        icon = BitmapDescriptorFactory.defaultMarker(
+                            BitmapDescriptorFactory.HUE_GREEN
                         )
-                    },
-                    onClick = {
-                        it.showInfoWindow()
-                        true
-                    },
-                    icon = BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_GREEN
                     )
-                )
+                }
+
             }
-        }
+        }}
+
     }
 }
