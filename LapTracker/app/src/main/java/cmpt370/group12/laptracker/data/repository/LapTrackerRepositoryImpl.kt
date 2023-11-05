37daa9a5bda0ba9714b8c14f43bf8ec3888f640c
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-04 18:46:50 -0600

    Created Implementation of Repository For BackEnd. This uses the interface, and overrides all the functions performing them directly on the database

---
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/repository/LapTrackerRepositoryImpl.kt
@@ -0,0 +1,153 @@
+package cmpt370.group12.laptracker.data.repository
+
+import cmpt370.group12.laptracker.data.dao.AchievementDao
+import cmpt370.group12.laptracker.data.dao.CommentDao
+import cmpt370.group12.laptracker.data.dao.MapPointDao
+import cmpt370.group12.laptracker.data.dao.RunsDao
+import cmpt370.group12.laptracker.data.dao.RuntimesDao
+import cmpt370.group12.laptracker.data.dao.TrackDao
+import cmpt370.group12.laptracker.data.mapper.toAchievement
+import cmpt370.group12.laptracker.data.mapper.toAchievementEntity
+import cmpt370.group12.laptracker.data.mapper.toComment
+import cmpt370.group12.laptracker.data.mapper.toCommentEntity
+import cmpt370.group12.laptracker.data.mapper.toMapPoint
+import cmpt370.group12.laptracker.data.mapper.toMapPointEntity
+import cmpt370.group12.laptracker.data.mapper.toRunTimes
+import cmpt370.group12.laptracker.data.mapper.toRunTimesEntity
+import cmpt370.group12.laptracker.data.mapper.toRuns
+import cmpt370.group12.laptracker.data.mapper.toRunsEntity
+import cmpt370.group12.laptracker.data.mapper.toTrack
+import cmpt370.group12.laptracker.data.mapper.toTrackEntity
+import cmpt370.group12.laptracker.domain.model.Achievement
+import cmpt370.group12.laptracker.domain.model.Comment
+import cmpt370.group12.laptracker.domain.model.MapPoint
+import cmpt370.group12.laptracker.domain.model.Runs
+import cmpt370.group12.laptracker.domain.model.Runtimes
+import cmpt370.group12.laptracker.domain.model.Track
+import cmpt370.group12.laptracker.domain.repository.LapTrackerRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+class LapTrackerRepositoryImpl(
+
+    private val achievementDao: AchievementDao,
+    private val commentDao: CommentDao,
+    private val mapPointDao: MapPointDao,
+    private val runsDao: RunsDao,
+    private val runtimesDao: RuntimesDao,
+    private val trackDao: TrackDao
+
+): LapTrackerRepository {
+
+
+    override suspend fun MapPoint_insert(mappoint: MapPoint) {
+        mapPointDao.MapPoint_insert(mappoint.toMapPointEntity())
+    }
+
+    override suspend fun MapPoint_delete(mappoint: MapPoint) {
+        mapPointDao.MapPoint_delete(mappoint.toMapPointEntity())
+    }
+
+    override fun MapPoint_getByTrackId(fromTrackId: Int): Flow<List<MapPoint>> {
+       return mapPointDao.MapPoints_getPointsByTrackId(fromTrackId).map { mappoint ->
+      mappoint.map { it.toMapPoint() }
+        }
+
+    }
+    override fun MapPoint_getAll(): Flow<List<MapPoint>> {
+        return mapPointDao.MapPoints_getAll().map { mappoint ->
+            mappoint.map { it.toMapPoint() }
+        }
+
+    }
+    override suspend fun Achievement_insert(achievement: Achievement){
+        achievementDao.Achievement_insert(achievement.toAchievementEntity())
+    }
+    override suspend fun Achievement_delete(achievement: Achievement){
+        achievementDao.Achievement_delete(achievement.toAchievementEntity())
+    }
+    override fun Achievement_getFinished(): Flow<List<Achievement>> {
+        return achievementDao.Achievement_getFinished().map { it ->
+            it.map { it.toAchievement() }}
+    }
+
+    override fun Achievement_getUnFinished(): Flow<List<Achievement>> {
+        return achievementDao.Achievement_getUnFinished().map { it ->
+            it.map { it.toAchievement() }}
+    }
+    override fun Achievement_getAll(): Flow<List<Achievement>>{
+        return achievementDao.Achievement_getAll().map { it ->
+            it.map { it.toAchievement() }}
+    }
+
+    override suspend fun Comment_insert(comment: Comment){
+        commentDao.Comment_insert(comment.toCommentEntity())
+    }
+    override suspend fun Comment_delete(comment: Comment){
+        commentDao.Comment_delete(comment.toCommentEntity())
+    }
+    override fun Comment_getCommentsFromTrackId(trackId: Int): Flow<List<Comment>> {
+        return commentDao.Comment_getCommentsFromTrackId(trackId).map { it ->
+            it.map { it.toComment() }
+        }
+    }
+    override fun Comment_getAllComments(): Flow<List<Comment>>{
+        return commentDao.Comment_getAllComments().map { it ->
+            it.map { it.toComment() }
+        }
+    }
+
+    override suspend fun Runs_insert(runs: Runs){
+        runsDao.Runs_insert(runs.toRunsEntity())
+    }
+    override suspend fun Runs_delete(runs: Runs){
+        runsDao.Runs_delete(runs.toRunsEntity())
+    }
+    override fun Runs_getByTrackId(trackId: Int): Flow<List<Runs>>{
+        return runsDao.Runs_getByTrackId(trackId).map { it ->
+            it.map { it.toRuns() }
+        }
+    }
+    override fun Runs_getAll(): Flow<List<Runs>>{
+        return runsDao.Runs_getAll().map { it ->
+            it.map { it.toRuns() }
+        }
+    }
+
+
+    override suspend fun RunTimes_insert(runtimes: Runtimes){
+        runtimesDao.RunTimes_insert(runtimes.toRunTimesEntity())
+    }
+    override suspend fun RunTimes_delete(runtimes: Runtimes){
+            runtimesDao.RunTimes_delete(runtimes.toRunTimesEntity())
+    }
+    override fun RunTimes_getByRunId(runId: Int): Flow<List<Runtimes>>{
+        return runtimesDao.RunTimes_getByRunId(runId).map { it ->
+            it.map { it.toRunTimes() }
+        }
+    }
+
+    override suspend fun Track_insert(track: Track){
+        trackDao.Track_insert(track.toTrackEntity())
+    }
+    override suspend fun Track_delete(track: Track){
+        trackDao.Track_delete(track.toTrackEntity())
+    }
+    override fun Track_getById(id: Int): Flow<List<Track>>{
+        return trackDao.Track_getById(id).map { it ->
+            it.map { it.toTrack() }
+        }
+    }
+    override fun Track_getAll(): Flow<List<Track>>{
+        return trackDao.Track_getAll().map { it ->
+            it.map { it.toTrack() }
+        }
+    }
+
+
+
+
+
+
+}
