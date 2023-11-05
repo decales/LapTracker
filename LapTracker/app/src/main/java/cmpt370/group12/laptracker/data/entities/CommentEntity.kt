d96737c3bc03bd1d26559f5fc0a702bfce76a338
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-04 18:28:24 -0600

    Seperated Entities From Single File Into Seperate Files Many Articles and Tutorials Point at this being better coding practice making them more reusable

---
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/entities/CommentEntity.kt
@@ -0,0 +1,18 @@
+package cmpt370.group12.laptracker.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Timestamp
+
+@Entity
+data class CommentEntity(
+    //id : This is the Primary Key For Database Entry into Table CommentEntity
+    @PrimaryKey
+    val id: Int? = null,
+    // fromTrackId: This is a reference to the primary Key "id" for the TrackEntity Table
+    val fromTrackId: Int,
+    // Comment is the String of the Comment itself
+    val comment: String,
+    //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
+    val timestamp: Timestamp
+)
---
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/data/entities/RunsEntity.kt
@@ -0,0 +1,20 @@
+package cmpt370.group12.laptracker.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Timestamp
+
+@Entity
+data class RunsEntity(
+    //id : This is the Primary Key For Database Entry into Table RunEntity
+    @PrimaryKey
+    val id: Int? = null,
+    // fromTrackId: This is a reference to the primary Key "id" for the TrackEntity Table
+    val fromTrackId: Int,
+    // starTime,endTime   these are the timestamps for the start and end of a race
+    // all times can be calculated by taking these times and comparing them to
+    // RunTimeEntities
+    //Todo: TimeStamp Might Not be The Correct Type, Must Verify This Later
+    val startTime: Timestamp,
+    val endTime: Timestamp
+)
