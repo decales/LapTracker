63ebea740b0fb12f30ed36d07d8a86ad033898ac
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-05 22:39:00 -0600

    Finally Removed the Default Functions.... LOL to that

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature7.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/Feature7.kt
@@ -2,14 +2,7 @@ package cmpt370.group12.laptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import android.annotation.SuppressLint
+
 import cmpt370.group12.laptracker.ui.presentation.MapScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,20 +16,4 @@ class Feature7 : ComponentActivity() {
 
         }
     }
-}
-
-@Composable
-fun Greeting8(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview8() {
-
-        Greeting8("Android")
-
 }
