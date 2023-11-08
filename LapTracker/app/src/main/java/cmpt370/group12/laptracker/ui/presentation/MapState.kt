d6f3f35b85f8dd60778d0f5e8234a031d299719b
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-08 01:19:15 -0600

    Added state variables to check if a flow is active and capture it's job (so we can terminate it)

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/ui/presentation/MapState.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/ui/presentation/MapState.kt
@@ -1,5 +1,6 @@
 package cmpt370.group12.laptracker.ui.presentation
 
+import android.location.Location
 import cmpt370.group12.laptracker.domain.model.Achievement
 import cmpt370.group12.laptracker.domain.model.Comment
 import com.google.maps.android.compose.MapProperties
@@ -8,19 +9,21 @@ import cmpt370.group12.laptracker.domain.model.Runs
 import cmpt370.group12.laptracker.domain.model.Runtimes
 import cmpt370.group12.laptracker.domain.model.Track
 import com.google.maps.android.compose.MapUiSettings
+import kotlinx.coroutines.Job
+
 /*
 Split Up The States.
 Went to a 3 Tier Approach.
 MapState : This will deal with Map Related States and will also store our current Lat/Long.
 TrackState: Track Related States.
-curentTrackId   our current track's primary key
+currentTrackId   our current track's primary key
 currentRunId  the current runs primary key
-mapPoints  the list of mappoints ONLY for the currentTrackId
+mapPoints  the list of mapPoints ONLY for the currentTrackId
 runtimes list of runtimes ONLY for the currentRunId
 AppState
 achievements list of all achievements (should be so small, might as well expose the whole thing)
 tracks list of all tracks (this is actually perfect to use to populate a dropdown)
-mapPoints list of all mappoints ( maybe for debugging)
+mapPoints list of all mapPoints ( maybe for debugging)
 run list of all runs ( maybe for debugging)
 runtimes list of all runtimes ( maybe for debugging)
 */
@@ -28,17 +31,21 @@ runtimes list of all runtimes ( maybe for debugging)
 data class MapState(
     val properties: MapProperties = MapProperties(),
     val uiSettings: MapUiSettings = MapUiSettings(),
-    val currentLatitude: Long = 0,
-    val currentLongitude: Long = 0
+    val currentLocation: Location? = Location(null),
+
 
 
 )
 data class TrackState(
     val currentTrackId: Int = 0,
     val currentRunId: Int = 0,
+    val currentTargetId: Int = 0,
+    val currentTargetDistance: Int = 0,
     val comments: List<Comment> = emptyList(),
     val mapPoints: List<MapPoint> = emptyList(),
-    val runtimes: List<Runtimes> = emptyList()
+    val runtimes: List<Runtimes> = emptyList(),
+    val runs: List<Runs> = emptyList(),
+
 )
 
 data class AppState(
@@ -46,7 +53,19 @@ data class AppState(
     val tracks: List<Track> = emptyList(),
     val mapPoints: List<MapPoint> = emptyList(),
     val runs: List<Runs> = emptyList(),
-    val runtimes: List<Runtimes> = emptyList()
+    val runtimes: List<Runtimes> = emptyList(),
+    val flowCurrentLocationActive: Boolean = false,
+    val flowCurrentLocationJob: Job? = null,
+    val flowCurrentTrackMapPointActive: Boolean = false,
+    val flowCurrentTrackMapPointJob: Job? = null,
+    val flowCurrentRunTimesActive: Boolean = false,
+    val flowCurrentRunTimesJob: Job? = null,
+    val flowTrackListActive: Boolean = false,
+    val flowTrackListJob: Job? = null,
+    val flowRunListByTrackIdActive: Boolean = false,
+    val flowRunListByTrackIdJob: Job? = null,
+    val flowCurrentTrackRunsListJob : Job? = null,
+    val flowCurrentTrackRunsListActive :Boolean = false,
 
     )
 
