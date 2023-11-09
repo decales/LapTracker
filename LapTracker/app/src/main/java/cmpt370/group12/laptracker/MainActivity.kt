66f7debc3f8d673fc046f93c9972ff8808504f6c
KRL872 <114636390+KRL872@users.noreply.github.com>
2023-11-09 04:17:28 -0600

    Set Startup Screen to StartupScreen

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/MainActivity.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/MainActivity.kt
@@ -3,8 +3,7 @@ package cmpt370.group12.laptracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import cmpt370.group12.laptracker.ui.composable.MapScreen
-import cmpt370.group12.laptracker.ui.composable.me
+import cmpt370.group12.laptracker.ui.composable.StartupScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,11 +14,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            MapScreen()
+            StartupScreen()
+            //ModeSelectScreen()
+            //MapScreen()
         }
     }
 }
+
 
 
 
