d1f5cf1488aa4c62603d9da2345dd486e4302786
Declan <Declan@DECLAN-PC>
2023-11-11 17:49:33 -0600

    Created achievements and statistics sub-containers

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/ProfileViewModel.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/ProfileViewModel.kt
@@ -10,19 +10,25 @@ class ProfileViewModel: ViewModel() {
     // TODO add all data and states values required for ProfileView composable functions
     // TODO (if applicable) retrieve data from database (model)
 
+    // Data classes
+
     data class ProfileTab(
         val text: String,
         val icon: Int,
     )
 
-    var currentTab by mutableIntStateOf(0)
+    // Variable and object
+
+    var currentPage by mutableIntStateOf(0)
 
     val profileTabs = listOf(
         ProfileTab("Statistics", R.drawable.ic_launcher_foreground),
         ProfileTab("Achievements", R.drawable.ic_launcher_foreground)
     )
 
-    fun setTab() {
-        currentTab = if (currentTab == 0) 1 else 0
+    // Methods
+
+    fun setPage(index: Int) {
+        currentPage = index
     }
 }
