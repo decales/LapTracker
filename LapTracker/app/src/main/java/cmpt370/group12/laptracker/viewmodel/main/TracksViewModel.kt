7aaf4bdfc4840a7af8a2044229ab65e01b02c7ad
Declan <Declan@DECLAN-PC>
2023-11-12 19:10:28 -0600

    Touched up achievements and tracks sections, fixed navbar insets padding

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/TracksViewModel.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/TracksViewModel.kt
@@ -7,17 +7,13 @@ class TracksViewModel :ViewModel() {
     // TODO add all data and states values required for TracksView composable functions
     // TODO (if applicable) retrieve data from database (model)
 
-    val trackCards = listOf( // TODO Dummy values, replace with array of database query result
-        TrackCard(0, "Name", "Location", R.drawable.ic_launcher_foreground),
-        TrackCard(0, "Name", "Location", R.drawable.ic_launcher_foreground),
-        TrackCard(0, "Name", "Location", R.drawable.ic_launcher_foreground),
-        TrackCard(0, "Name", "Location", R.drawable.ic_launcher_foreground),
-    )
-
     data class TrackCard(
         val id: Int, // track reference in database
         val name: String,
         val location: String,
         val mapSnippet: Int
     )
+
+    // TODO Dummy values, replace with array of database query result
+    val trackCards = List(12) { TrackCard(0, "Name", "Location", R.drawable.ic_launcher_foreground) }
 }
