5a4fd8314696fad43fd4a3058124bf3731f08759
Riley <rileykensmith@gmail.com>
2023-11-15 17:46:21 -0600

    StartView now has better implementation of lap tracking

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/view/main/StartView.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/view/main/StartView.kt
@@ -2,7 +2,6 @@ package cmpt370.group12.laptracker.view.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,15 +12,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cmpt370.group12.laptracker.viewmodel.main.StartViewModel
+import cmpt370.group12.laptracker.viewmodel.main.StartViewModel.Point
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class StartView(
@@ -35,14 +39,8 @@ class StartView(
     fun View() {
         // TODO build view from class defined composable functions.
         // TODO initialize necessary view data in viewmodel/main/StartViewModel.kt. Data is accessed through constructor var 'viewModel'
-        //Row()
-        //{
-            Header()
-        //}
-        //Row()
-        //{
-                mainScreen()
-        //}
+        Header()
+        trackingView()
     }
 
     @Composable
@@ -59,98 +57,145 @@ class StartView(
         }
     }
 
-
     @Composable
-    fun mainScreen() {
-        val points = remember { mutableStateListOf<Pair<Double,Double>>() } // Remember list of points
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Row (
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    if (points.isNotEmpty()) {
-                        Text(text = "Would you like to set a segment?")
-                    }
-                }
-            }
-        }
+    fun trackingView() {
+        val points = remember { mutableStateListOf<Point>() } // Remember list of points
+        var setToggle by remember { mutableStateOf(false) } // Remember set button toggle
+
         Box (
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            Column(
+            Column (
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Box( // Container to display location points
+                Box ( // Container to display location points
                     modifier = Modifier
                         .padding(bottom = 100.dp)
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(top = 10.dp)
                     ) {
+
                         points.forEach { point ->
-                            Text(text = point.toString())
+                            Text(text = point.name)
                         }
                     }
                 }
-                if (points.isNotEmpty()) {
-                    getAnotherSegmentButton(points)
-                }
-                else {
-                    setStartLocationButton(points)
+                ToggleSetPointsButton(points, setToggle) { setToggle = !setToggle }
+
+                if (points.isNotEmpty() && !setToggle) {
+                    TrackingButton(points)
                 }
             }
         }
     }
 
+
     @Composable
-    fun setStartLocationButton(points: SnapshotStateList<Pair<Double, Double>>) {
+    fun ToggleSetPointsButton(
+        points: SnapshotStateList<Point>,
+        setToggle: Boolean,
+        onClick: () -> Unit
+    ) {
+        val text = if (points.isEmpty() && !setToggle) "Set points" else if (!setToggle) "Edit points" else "Done"
+
+        Button(onClick = onClick) {
+            Text(text = text)
+        }
+        if (setToggle) { // When button is toggled on, display another button to set (and undo) a point
+            SetPointButton(points)
+        }
+    }
+
+
+    @Composable
+    fun SetPointButton (
+        points: SnapshotStateList<Point>
+    ) {
         val scope = CoroutineScope(Dispatchers.Main)
         var isLoading by remember { mutableStateOf(false) }
-        isLoading = true
+        val text = if (points.isEmpty()) "Set start" else "Set point"
+
         Button( // Set a new point and add it to points array
             onClick = {
                 isLoading = true
                 scope.launch {
-                    points.add(viewModel.getAverageLocation())
+                    val pointID = if (points.isEmpty()) "Start" else "L${points.size}"
+                    points.add(Point(viewModel.getAverageLocation(), pointID,false))
                     isLoading = false
                 }
             }
         ) {
-            Text("Set Start Location")
+            Text(text = text)
+        }
+        if (points.isNotEmpty()) {
+            Button( // Remove last point entry from points array
+                onClick = { points.removeLast() }
+            ) {
+                Text(text = "Undo")
+            }
+        }
+        if (isLoading) {
+            Text (
+                text = "Setting point...",
+                modifier = Modifier
+                    .padding(top = 20.dp))
         }
     }
 
     @Composable
-    fun getAnotherSegmentButton(points: SnapshotStateList<Pair<Double, Double>>) {
-        val scope = CoroutineScope(Dispatchers.Main)
-        var isLoading by remember { mutableStateOf(false) }
-        isLoading = true
-        Button( // Set a new point and add it to points array
-            onClick = {
-                isLoading = true
-                scope.launch {
-                    points.add(viewModel.getAverageLocation())
-                    isLoading = false
+    fun TrackingButton (
+        points: SnapshotStateList<Point>,
+    ) {
+        var isToggled by remember { mutableStateOf(false) }
+        val color = if (!isToggled) Color(0, 153, 0) else Color(179, 0, 89)
+        val text = if (!isToggled) "Start Tracking" else "Stop Tracking"
+        var thread by remember { mutableStateOf<Job?>(null) }
+        val scope = rememberCoroutineScope()
+
+        // Tracking UI variables
+        var distance by remember { mutableStateOf(0.0) }
+        var laps by remember { mutableStateOf(0) }
+        var next by remember { mutableStateOf("")}
+
+        Button(onClick = {
+
+            isToggled = !isToggled
+
+            if (isToggled) { // Start lap tracking
+                thread = scope.launch {
+                    while (thread?.isActive == true) {
+                        points.forEach { point ->
+                            next = point.name
+                            viewModel.getProximityFlow(point.latlon).first { d -> // Emit from flow until within 2 meters
+                                distance = d // Update UI
+                                d < 2.0
+                            }
+                        }
+                        laps += 1 // All points have been reached, +1 lap
+                    }
                 }
             }
-        ) {
-            Text("Set a Segment")
+            else { // Reset values and
+                distance = 0.0
+                laps = 0
+                thread?.cancel()
+            }
+        }) {
+            Text(text = text, color = color)
         }
 
-        Button(
-            onClick = {
-
-            }
-        ) {
-            Text("Finished")
+        if (thread?.isActive == true) {
+            Text (
+                text = "$next is $distance m away",
+                modifier = Modifier
+                    .padding(top = 20.dp)
+            )
+            Text(text = "Laps: $laps")
         }
     }
 }
