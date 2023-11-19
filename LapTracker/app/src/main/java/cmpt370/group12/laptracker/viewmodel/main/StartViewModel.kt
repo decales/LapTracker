990545181fc942971001b95ae2003c3fd55e7ee3
Riley <rileykensmith@gmail.com>
2023-11-19 13:33:41 -0600

    Moved all variables to viewModel

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/StartViewModel.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/main/StartViewModel.kt
@@ -1,13 +1,34 @@
 package cmpt370.group12.laptracker.viewmodel.main
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import cmpt370.group12.laptracker.model.LocationClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 
 class StartViewModel(locationClient: LocationClient) : ViewModel() {
     // TODO add all data and states values required for SettingsView composable functions
     // TODO (if applicable) retrieve data from database (model)
     var location = locationClient
+    val points = mutableStateListOf<Point>()
+    var setToggle = mutableStateOf(false)
+    var textToggleSetPoints = "Set points"
+    val scope = CoroutineScope(Dispatchers.Main)
+    var textSetPoints = "Set start"
+    var isLoading = mutableStateOf(false)
+    var isToggled = mutableStateOf(false)
+    var color = Color(0, 153, 0)
+    var textTracking = "Start Tracking"
+    var thread = mutableStateOf<Job?>(null)
+
+    // Tracking UI variables
+    var distance = mutableStateOf(0.0)
+    var laps = mutableStateOf(0)
+    var next = mutableStateOf("")
 
     data class Point (
         val latlon: Pair<Double, Double>,
