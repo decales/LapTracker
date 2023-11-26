a1b6e86ff0518833dfc3766781fcded1c96f0d49
Damon <dld316@usask.ca>
2023-11-26 15:27:27 -0600

    Reapply "Merge branch 'main' into UI_Analysis"
    This reverts commit c220440986b5f941cf953eec202886b1c45eb910.

--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/model/data/dao/TrackDao.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/model/data/dao/TrackDao.kt
@@ -27,7 +27,7 @@ interface TrackDao {
     fun Track_getAllFlow(): Flow<List<TrackEntity>>
 
     @Query("SELECT * FROM trackentity")
-    fun Track_getAll(): List<TrackEntity>
+    suspend fun Track_getAll(): List<TrackEntity>
 }
 
 
--- LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/SettingsViewModel.kt
+++ LapTracker/app/src/main/java/cmpt370/group12/laptracker/viewmodel/SettingsViewModel.kt
@@ -1,8 +1,14 @@
 package cmpt370.group12.laptracker.viewmodel
 
 import androidx.lifecycle.ViewModel
+import cmpt370.group12.laptracker.model.domain.repository.LapTrackerRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SettingsViewModel(backend: GlobalViewModel) : ViewModel() {
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val db: LapTrackerRepository
+): ViewModel() {
     // TODO add all data and states values required for SettingsView composable functions
     // TODO (if applicable) retrieve data from database (model)
 }
